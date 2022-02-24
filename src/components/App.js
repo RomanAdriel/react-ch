72110122c183e6b0bf2fff890c4c9fd6a0becf70
Roman Diaz <roman.diaz09@gmail.com>
2022-02-24 15:23:40 -0300

    Desafio 2

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const App = () => {
+    return (<> <Header/> < Footer /> </>)
+}
+
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <a href="#">Categoría 1</a>
+            <a href="#">Categoría 2</a>
+            <a href="#">Categoría 3</a>
+        </nav>
+    )
+}
+
+export default NavBar
